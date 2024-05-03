0af18295f0ffdda032d3a0648c5e873850375db9
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-05-03 21:49:40 +0300

    edited configuration

--- LinkedInAuth.WebAPI/Program.cs
+++ LinkedInAuth.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.LogoutPath = "/logout";
+        options.Cookie.Name = "auth-token";
+    });
+
+
+builder.Services.AddAuthentication().AddOAuth("LinkedIn", options =>
+{
+    options.ClientId = builder.Configuration["LinkedIn:ClientId"];
+    options.ClientSecret = builder.Configuration["LinkedIn:ClientSecret"];
+});
 
 var app = builder.Build();
 
@@ -18,7 +36,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapDefaultControllerRoute();
 
 app.MapControllers();
 
