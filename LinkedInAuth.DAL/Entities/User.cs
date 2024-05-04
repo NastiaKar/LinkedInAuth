0a16db3ac94cfcd4f3f1a1359297053cf1b354ae
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-05-04 08:43:18 +0300

    added 'first name' prop to User entity

--- LinkedInAuth.DAL/Entities/User.cs
+++ LinkedInAuth.DAL/Entities/User.cs
@@ -7,6 +7,9 @@ public class User
     [JsonPropertyName("id")]
     public string Id { get; set; } = null!;
     
+    [JsonPropertyName("localizedFirstName")]
+    public string LocalizedFirstName { get; set; } = null!;
+    
     [JsonPropertyName("profilePicture")]
     public ProfilePicture ProfilePicture { get; set; }
 }
