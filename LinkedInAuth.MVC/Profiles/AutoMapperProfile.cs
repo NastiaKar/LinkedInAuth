cef000c80d6708fe121564e8257b0ff0e6f4f780
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-05-06 12:35:26 +0300

    added AutoMapper

---
+++ LinkedInAuth.MVC/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LinkedInAuth.MVC.Models;
+using Newtonsoft.Json.Linq;
+
+namespace LinkedInAuth.MVC.Profiles;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<JObject, UserProfileViewModel>()
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src["email"].Value<string>()))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src["name"].Value<string>()))
+            .ForMember(dest => dest.Picture, opt => opt.MapFrom(src => src["picture"].Value<string>()));
+    }
+}
