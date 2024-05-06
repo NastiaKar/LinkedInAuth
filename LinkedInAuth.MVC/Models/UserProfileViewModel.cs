573d1db97fa577515ffb0bf7884f53f8fccb586b
IK-12 Настя Карпюк <nastia.karpyuk@outlook.com>
2024-05-06 12:34:53 +0300

    added ViewModels

---
+++ LinkedInAuth.MVC/Models/UserProfileViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkedInAuth.MVC.Models;
+
+public class UserProfileViewModel
+{ 
+    public string Email { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Picture { get; set; } = null!;
+}
